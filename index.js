aa04048ed1f3734821891d2604f7dfe447a66a23
Andrew Sprague <andrewsprague@outlook.com>
2018-06-25 22:47:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`
+  return `Happy Holidays, ${name}!`
 }
 
-function happyHolidayTo (name, holiday) {
+function happyHolidayTo (holiday, name) {
   return `Happy ${holiday}, ${name}`
 }
 
-function holidayCountdown(days, holiday) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}`
 }
